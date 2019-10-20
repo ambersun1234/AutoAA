08271d97c41863eb84b8b19b99ba188930748243
ambersun <ambersun1019.shawn@gmail.com>
2019-10-20 18:42:39 +0800

    Add user config

---
+++ config/aaConfig.py
@@ -0,0 +1,7 @@
+class aaConfig:
+    loginModalFieldClass1    = "sidenav-toggle"
+    loginModalFieldClass2    = "iconcolorsignup"
+    loginEmailFieldId        = "sso-login-email-input"
+    loginPasswordFieldId     = "sso-login-password-input"
+    loginButtonFieldClass    = "sso-button"
+    loginButtonFieldType     = "submit"
